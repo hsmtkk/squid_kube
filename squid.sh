4425a1ee2a56a55a95023ca909ce0dc895ccb9a2
hsmtkk <hsmtkk@gmail.com>
2020-04-29 22:23:42 +0000

    add c-icap container

--- squid.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --rm -it --volume /home/ec2-user/squid_kube/squid.conf:/usr/local/etc/squid.conf:ro hsmtkk/squid:4.11
-
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -x 127.0.0.1:8000 http://www.example.org
+
