ae4d9c4d9c05f0e3ab7ef043f206b28f11154ab6
hsmtkk <hsmtkk@gmail.com>
2020-04-30 00:57:07 +0000

    create log directory

--- clamav/entrypoint.sh
+++ clamav/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir -p /usr/local/clamav/var/log
 tail -F /usr/local/clamav/var/log/clamd.log &
 /usr/local/clamav/bin/freshclam
 /usr/local/clamav/sbin/clamd --foreground --config-file=/usr/local/clamav/etc/clamd.conf
