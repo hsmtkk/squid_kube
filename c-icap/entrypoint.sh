015fed0cc50fa8f446decc4402bee7eaddf96c10
hsmtkk <hsmtkk@gmail.com>
2020-04-30 21:45:25 +0000

    remove non used directory

--- c-icap/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-tail -F /usr/local/c-icap/var/log/access.log &
-tail -F /usr/local/c-icap/var/log/server.log &
-tail -F /usr/local/c-icap/var/log/c-icap-access-url_check.log &
-tail -F /usr/local/c-icap/var/log/c-icap-access-vscan.log &
-/usr/local/c-icap/bin/c-icap -N -f /usr/local/c-icap/etc/c-icap.conf
-
